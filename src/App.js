8fa8c3e7b3b797ed4fa7c717747f4ce6222cff9e
charlessonbo <bocharlesson@gmail.com>
2022-01-27 22:08:19 +0800

    change App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React (TEST PAGE AGAIN)
         </a>
       </header>
     </div>
